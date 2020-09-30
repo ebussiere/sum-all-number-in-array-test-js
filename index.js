58670ca1641fab5d25c2faa2028f9910125749ce
ebussiere <eddy@embtech.ca>
2020-09-30 14:56:40 +0000

    final

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let result = 0;
+  function inner(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(i)) {
+        inner(i);
+      } else {
+        result = result + i;
+      }
+    };
+  }
+  inner(array);
+  return result;
 }
 
+
+
+
+// function sumItems(array) {
+//   for (const a of array) {
+//     if (Array.isArray(a)) {
+//       return sumItems(a);
+//     } else {
+//       console.log(a);
+//       //sum = sum + a;
+//     }
+//   }
+// }
 module.exports = sumItems;
